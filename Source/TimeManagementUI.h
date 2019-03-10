4380b704139cbf869aec69e4bfac647ef3197692
Jonas Maier <jonas.maier@adabay.rocks>
2019-03-10 03:06:08 +0100

    Add framework project

---
+++ Source/TimeManagementUI.h
@@ -0,0 +1,18 @@
+//
+//  TimeManagementUI.h
+//  TimeManagementUI
+//
+//  Created by Jonas Maier on 10.03.19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimeManagementUI.
+FOUNDATION_EXPORT double TimeManagementUIVersionNumber;
+
+//! Project version string for TimeManagementUI.
+FOUNDATION_EXPORT const unsigned char TimeManagementUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimeManagementUI/PublicHeader.h>
+
+
